cd6171cb55d48b97588f7cc767e9edcd4e45d32e
gowtham-u <er.gowtham.umapathy@gmail.com>
2021-11-09 13:53:06 +0530

    commit

---
+++ arithmetic.py
@@ -0,0 +1,38 @@
+first_value = input("Enter the val ")
+second_value = input("Enter the val ")
+first_value = int(first_value)
+second_value = int(second_value)
+addition = first_value + second_value
+print("addition of two numbers", addition)
+
+
+sub_first_value = input("Enter the val ")
+sub_second_value = input("Enter the val ")
+sub_first_value = int(sub_first_value)
+sub_second_value = int(sub_second_value)
+subtraction = sub_first_value - sub_second_value
+print("subtraction of two numbers", subtraction)
+
+
+mul_first_value = input("Enter the val ")
+mul_second_value = input("Enter the val ")
+mul_first_value = int(mul_first_value)
+mul_second_value = int(mul_second_value)
+multiply = mul_first_value * mul_second_value
+print("multiply of two numbers", multiply)
+
+
+div_first_value = input("Enter the val ")
+div_second_value = input("Enter the val ")
+div_first_value = int(div_first_value)
+div_second_value = int(div_second_value)
+division = div_first_value / div_second_value
+print("division of two numbers", division)
+
+
+mod_first_value = input("Enter the val")
+mod_second_value = input("Enter the val")
+mod_first_value = int(mod_first_value)
+mod_second_value = int(mod_second_value)
+modulus = mod_first_value % mod_second_value
+print("mod of two numbers", modulus)
